a54403673001d3f5040e71268ff7806e73f3e5c5
Ana G Gonzalez <63682168+Anigabi@users.noreply.github.com>
2021-10-18 23:59:24 +0200

    Quito console.log de Rigoberto

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  console.log("Hello Rigo from the console!");
 
   const who = ["my boyfriend", "I", "my bestie", "my chicken"];
   const act = ["fall", "eat", "wet"];
